bd946355db635b4bf3cfc346bb2e68fbf02bdf2d
dimk00z <dimk00z@gmail.com>
2022-01-17 15:31:37 +0300

    Add user model

---
+++ notification_api/src/models/user.py
@@ -0,0 +1,17 @@
+from datetime import date
+from typing import Any
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    user_id: UUID
+    user_name: str
+    user_last_name: str
+    email: str | None
+    phone_number: str | None
+    telegram_user_name: str | None
+    location: str | None
+    birthdate: date | None
+    additional_data: Any | None
