36a08f159255e3ab3113ce1d9de32ec05d702c7c
dimk00z <dimk00z@gmail.com>
2022-01-18 16:05:26 +0300

    Fix matching

--- notification_api/src/dependencies/user_storage_dependencies.py
+++ notification_api/src/dependencies/user_storage_dependencies.py
@@ -6,18 +6,17 @@ user_service: BaseUserService | None = None
 
 
 def set_user_service(service_name: str, services_settings: UserSettings) -> BaseUserService:
+    if service_name == "AUTH_USER_SERVICE":
+        return AuthUserService(services_settings.auth_user_settings)
+    elif service_name == "DB_USER_SERVICE":
+        return AuthUserService(services_settings.bd_user_settings)
+    elif service_name == "DEBUG_USER_SERVICE":
+        if get_settings().app.is_debug:
+            from services.user import DebugUserService
 
-    match service_name:
-        case "AUTH_USER_SERVICE":
-            return AuthUserService(services_settings.auth_user_settings)
-        case "DB_USER_SERVICE":
-            return AuthUserService(services_settings.bd_user_settings)
-        case "DEBUG_USER_SERVICE":
-
-            if get_settings().app.is_debug:
-                from services.user import DebugUserService
-
-                return DebugUserService()
+            return DebugUserService()
+        else:
+            raise RuntimeError("Debug service should been used only with is_debug=True")
 
 
 def get_user_service() -> BaseUserService | None:
